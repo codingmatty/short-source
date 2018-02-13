3c6c73cf1899eb9c594d76e4e612655161a5df40
Matt Jacobs <hello@mattjdev.com>
2018-02-12 22:57:39 -0600

    Update front end to use SCSS and look slightly prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import autobind from 'react-autobind';
 import logo from './logo.svg';
-import './App.css';
+import './app.css';
 
 class App extends Component {
   state = { url: '' };
@@ -24,28 +24,40 @@ class App extends Component {
     })
       .then((res) => {
         if (!res.ok) {
-          throw new Error();
+          return res.json().then((err) => Promise.reject(err));
         }
         return res.json();
       })
-      .then(({ url }) => this.setState({ message: url }))
-      .catch(() => this.setState({ message: 'ERROR!' }));
+      .then(({ url }) => this.setState({ shortUrl: url, error: null }))
+      .catch(({ error }) => this.setState({ shortUrl: null, error }));
   }
 
   render() {
-    const { message } = this.state;
+    const { shortUrl, error } = this.state;
 
     return (
-      <div className="App">
-        <form onSubmit={this.onSubmit}>
-          <label htmlFor="url">Url to Shrink</label>
+      <div className="app">
+        <form className="url-form" onSubmit={this.onSubmit} autoComplete="off">
+          <label htmlFor="url" className="url-label">
+            Url to Shrink
+          </label>
           <input
             id="url"
             type="text"
+            className="url-input"
             onChange={({ target }) => this.setState({ url: target.value })}
           />
+          <button className="url-submit">Submit</button>
+          {shortUrl && (
+            <p className="url-result">
+              The resulting URL is:{' '}
+              <a href={shortUrl} rel="noopener noreferrer" target="_blank">
+                {shortUrl}
+              </a>
+            </p>
+          )}
+          {error && <p className="url-error">{error}</p>}
         </form>
-        <div>{message}</div>
       </div>
     );
   }
