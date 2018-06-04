e94a82b9f316f478453680e2e09cc7756fecc173
Matt Jacobs <hello@mattjdev.com>
2018-06-04 12:39:38 -0500

    Update storing API Key within an env variable for authentication

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const apiKey = process.env.REACT_APP_FIREBASE_API_KEY;
+
+module.exports = {
+  apiKey,
+  authDomain: 'moniker-app.firebaseapp.com',
+  databaseURL: 'https://moniker-app.firebaseio.com',
+  projectId: 'moniker-app',
+  storageBucket: 'moniker-app.appspot.com',
+  messagingSenderId: '5433795506'
+};
